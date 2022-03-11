91515da4891fa13c08d7f792556427871d2b601b
gbengarock <temidayoolugbenga@yahoo.com>
2022-03-10 20:47:20 -0800

    script that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#!/bin/bash
-#include studio.h
+#include <studio.h>
 
-/*
-* main - entry point
-*Return: always 0
-
+/**
+*main -  prints the phrase "Programming is like building a multilingual puzzle"
+*Return: 0 if exited properly, non-zero otherwise
 */
 int main (void)
 {
-
-puts ("\"programming is like building a multilingual puzzle")
-
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
