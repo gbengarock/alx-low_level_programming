7779e6e364998f80ceb070465d041da0318c132e
gbengarock <temidayoolugbenga@yahoo.com>
2022-03-10 21:28:35 -0800

    script that prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include  <stdio.h>
+#include <stdio.h>
 /**
 *main - Entry point
 *
 *Return: always 0
 */
-int main (void)
+int main(void)
 {
-printf ("with proper grammar, but the propeoutcome is a piece of art, \n");
-return (0);
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return(0);
 }
