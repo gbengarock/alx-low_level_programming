a651e0be3f5e744fb16a8cfef7f9b4abbd44f2b7
gbengarock <temidayoolugbenga@yahoo.com>
2022-03-10 21:13:20 -0800

    script that prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+/**
+*main - Entry point
+*
+*Return: always 0
+*/
+int main (void)
+{
+printf ("with proper grammar, but the propeoutcome is a piece of art, \n");
+return (0);
+}
