3587a81c6948ff486ecc4500a8c53210982fe7fb
gbengarock <temidayoolugbenga@yahoo.com>
2022-03-12 13:42:58 -0800

    corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include  <stdio.h>
-
+#include <stdio.h>
 /**
-*main - prints the phrase "Programming is like building a multilingual puzzle"
-*Return: 0 if exited properly,  non-zero otherwise 
-*/
-int main (void)
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
