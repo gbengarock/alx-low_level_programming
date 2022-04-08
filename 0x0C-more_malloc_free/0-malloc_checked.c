76c769141808f088f7aadaecb487201212f42daf
gbengarock <temidayoolugbenga@yahoo.com>
2022-04-08 15:25:05 -0700

    new file added

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -20,16 +20,16 @@ void *malloc_checked(unsigned int b)
 
 {
 
-        int *m = malloc(b);
+	int *m = malloc(b);
 
 
 
-        if (m == 0)
+	if (m == 0)
 
-                exit(98);
+		exit(98);
 
 
 
-        return (m);
+	return (m);
 
 }
