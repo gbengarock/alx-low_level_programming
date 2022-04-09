6f9db1d614cc682c7a339f83cc7024ba5665fd17
gbengarock <temidayoolugbenga@yahoo.com>
2022-04-09 04:33:10 -0700

    new changes

--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-Learning malloc and free
-=======
 Learn more malloc and free
->>>>>>> 76c769141808f088f7aadaecb487201212f42daf
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
-<<<<<<< HEAD
 #define MAIN_H
 
 #include <stdlib.h>
@@ -14,35 +13,4 @@ int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
 
-=======
-
-#define MAIN_H
-
-
-
-#include <stdlib.h>
-
-
-
-void _puts(char *str);
-
-void *malloc_checked(unsigned int b);
-
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-
-void *_calloc(unsigned int nmemb, unsigned int size);
-
-int *array_range(int min, int max);
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);        
-
-int _putchar(char c);
-
-int _atoi(const char *s);
-
-void print_int(unsigned long int n);
-
-
-
->>>>>>> 76c769141808f088f7aadaecb487201212f42daf
 #endif
