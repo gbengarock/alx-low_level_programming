81c15c64910e0743771076135b63441534678b0d
Apo Olugbenga <99582201+gbengarock@users.noreply.github.com>
2022-06-11 01:00:39 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,18 +22,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		head = *h;
 		i = 1;
-												if (head != NULL)
-													while (head->prev != NULL)
+		if (head != NULL)
+			while (head->prev != NULL)
 				head = head->prev;
-													while (head != NULL)
+		while (head != NULL)
+		{
+			if (i == idx)
 			{
-				if (i == idx)
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
 				{
-					if (head->next == NULL)
-						new = add_dnodeint_end(h, n);
-					else
-					{
-						new = malloc(sizeof(dlistint_t));
+					new = malloc(sizeof(dlistint_t));
 					if (new != NULL)
 					{
 						new->n = n;
